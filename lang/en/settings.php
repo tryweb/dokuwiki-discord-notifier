dde17a12cae50240141c4ea9f6c54750b29e0ba1
Thierry Nicola <thierry@eevol.lu>
2015-03-17 16:31:37 +0100

    Update settings.php

--- lang/en/settings.php
+++ lang/en/settings.php
@@ -5,7 +5,8 @@
  * @author Brendon Rapp <brendon@jaguardesignstudio.com>
  */
 
-  $lang['hipchat_token']      = "HipChat API token";
-  $lang['hipchat_room']       = "HipChat room name";
-  $lang['hipchat_name']       = "Notification user name";
-  $lang['hipchat_namespaces'] = "Namespace filter: notify only on these namespaces (comma-separated)";
+  $lang['slackhq_token']      = "Slack API token";
+  $lang['slackhq_channel']    = "Slack channel name";
+  $lang['slackhq_name']       = "Notification user name";
+  $lang['slackhq_namespaces'] = "Namespace filter: notify only on these namespaces (comma-separated)";
+  $lang['slackhq_team']       = "Slack Team";
