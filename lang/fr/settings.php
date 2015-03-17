b06891fde671e67aeeaeae43b45ea205c190373d
Thierry Nicola <thierry@eevol.lu>
2015-03-17 16:34:07 +0100

    Update settings.php

--- lang/fr/settings.php
+++ lang/fr/settings.php
@@ -2,10 +2,11 @@
 /**
  * Configuration setting title strings
  *
- * @author Brendon Rapp <brendon@jaguardesignstudio.com>
+ * @author Thierry <thierry@eevol.lu>
  */
 
-  $lang['hipchat_token']      = "Token de l'API HipChat";
-  $lang['hipchat_room']       = "Nom de la room HipChat";
-  $lang['hipchat_name']       = "Nom d'utilisateur pour les notifications";
-  $lang['hipchat_namespaces'] = "Filtre de catégories : envoie une notification uniquement depuis ces catégories (séparées par une virgule)";
+  $lang['slackhq_token']      = "Token de l'API Slack";
+  $lang['slackhq_channel']    = "Nom du channel Slack";
+  $lang['slackhq_name']       = "Nom d'utilisateur pour les notifications";
+  $lang['slackhq_namespaces'] = "Filtre de catégories : envoie une notification uniquement depuis ces catégories (séparées par une virgule)";
+  $lang['slackhq_team']       = "Slack Team";
