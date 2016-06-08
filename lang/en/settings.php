3335b995fbf8eb18828117b584290483e7d7b317
mallchin <mallchin@mac.com>
2016-06-08 23:05:26 +0100

    Update settings.php

--- lang/en/settings.php
+++ lang/en/settings.php
@@ -5,8 +5,8 @@
  * @author Brendon Rapp <brendon@jaguardesignstudio.com>
  */
 
-  $meta['slackhq_icon']       = "Slack Icon ";
-  $meta['slackhq_channel']    = "Slack channel name";
-  $meta['slackhq_name']       = "Notification user name";
+  $meta['slackhq_icon']       = "Slack Icon";
+  $meta['slackhq_channel']    = "Slack Channel";
+  $meta['slackhq_name']       = "Notification user";
   $meta['slackhq_namespaces'] = "Namespace filter: notify only on these namespaces (comma-separated)";
   $meta['slackhq_webhook']    = "URL for Incoming Webhook";
