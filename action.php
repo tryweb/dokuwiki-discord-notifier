33dc31a8d2ae1765a8c5498d9ae1d8a13e96d8f0
mallchin <mallchin@mac.com>
2016-06-09 17:27:26 +0100

    Update action.php

--- action.php
+++ action.php
@@ -1,153 +1,172 @@
 <?php
 /**
- * DokuWiki Plugin Slack Integration (Action Component)
+ * DokuWiki Plugin Slack Notifier (Action Component)
  *
  * @license GPL 2 http://www.gnu.org/licenses/gpl-2.0.html
- * @author  Jeremy Ebler <jebler@gmail.com> 2011-09-29
  *
- * DokuWiki log: https://github.com/cosmocode/log.git
- * @author  Adrian Lang <lang@cosmocode.de> 2010-03-28
- *
-
  */
 
-// must be run within Dokuwiki
 if (!defined('DOKU_INC')) die();
+
+require_once (DOKU_INC.'inc/changelog.php');
 
 if (!defined('DOKU_LF')) define('DOKU_LF', "\n");
 if (!defined('DOKU_TAB')) define('DOKU_TAB', "\t");
 if (!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
 
-require 'vendor/autoload.php';
+class action_plugin_slacknotifier extends DokuWiki_Action_Plugin {
 
-class action_plugin_slackhq extends DokuWiki_Action_Plugin {
+	function register(Doku_Event_Handler $controller) {
+		$controller->register_hook('ACTION_ACT_PREPROCESS', 'BEFORE', $this, 'handle_action_act_preprocess');
+	}
 
-    function register(Doku_Event_Handler $controller) {
-       $controller->register_hook('ACTION_ACT_PREPROCESS', 'BEFORE', $this, 'handle_action_act_preprocess');
-    }
+	function handle_action_act_preprocess(Doku_Event $event, $param) {
+		if (isset($event->data['save'])) {
+			$this->handle();
+		}
+		return;
+	}
 
-    function handle_action_act_preprocess(Doku_Event $event, $param) {
-        global $lang;
-        if (isset($event->data['save'])) {
-            //if ($event->data['save'] == $lang['btn_save']) {
-                $this->handle();
-            //}
-        }
-        return;
-    }
+	private function handle() {
 
-    private function handle() {
-		global $SUM;
-        global $INFO;
 		global $conf;
+		global $ID;
+		global $INFO;
+		global $SUM;
 
-        /* Namespace filter */
-        $ns = $this->getConf('slackhq_namespaces');
-        if (!empty($ns)) {
-            $namespaces = explode(',', $ns);
-            $current_namespace = explode(':', $INFO['namespace']);
-            if (!in_array($current_namespace[0], $namespaces)) {
-                return;
-            }
-        }
+		// filter by namespaces
+		$ns = $this->getConf('namespaces');
+		if (!empty($ns)) {
+			$namespaces = explode(',', $ns);
+			$current_namespace = explode(':', $INFO['namespace']);
+			if (!in_array($current_namespace[0], $namespaces)) {
+				return;
+			}
+		}
 
-        $fullname = $INFO['userinfo']['name'];
-        $username = $INFO['client'];
-        $page     = $INFO['namespace'] . $INFO['id'];
-        $summary  = $SUM;
-        $minor    = (boolean) $_REQUEST['minor'];
+		// title
+		$fullname = $INFO['userinfo']['name'];
+		$page     = $INFO['namespace'] . $INFO['id'];
+		$title    = "{$fullname} updated page <{$this->urlize()}|{$INFO['id']}>";
 
- 
-        $icon = $this->getConf('slackhq_icon');
-        $channel = $this->getConf('slackhq_channel');
-        $from = $this->getConf('slackhq_name');
+		// compare changes
+		$changelog = new PageChangeLog($ID);
+		$revArr = $changelog->getRevisions(-1, 1);
+		if (count($revArr) == 1) {
+			$title .= " (<{$this->urlize($revArr[0])}|Compare changes>)";
+		}
 
-        $webhook = $this->getConf('slackhq_webhook');
+		/*
+		// attachement
+		$summary  = $SUM;
 
+                // payload
+                $data = array(
+                        "text"                  =>  $title,
+                        "attachments"           =>  array(array(
+                                "fallback"      => 'Change summary',
+                                "color"         => '#333',
+                                "title"         => $INFO['id'],
+                                "title_link"    => $this->urlize(),
+                                "text"          => $summary,
+                                "author"        => $fullname
+                        ))
+                );
+		*/
 
-        $say = '' . $fullname . ' updated the WikiPage <'. $this->urlize() . '|' . $INFO['id'] . '>';
-        //if ($minor) $say = $say . ' ['.$this->getLang('slackhq_minor').']';
-        //if ($summary) $say = $say . ' / ' . $summary . '';
+		// title
+                $data = array(
+                        "text"                  =>  $title
+                );
 
+		// summary
+		if (!empty($SUM)) {
+			$data['attachments'] = array(array(
+                                "fallback"      => "Change summary",
+				"title"		=> "Summary",
+                                "text"          => "{$SUM}\n- {$fullname}"
+			));
+		}
 
-        $data = "payload=" . json_encode(array(
-                "channel"       =>  "#{$channel}",
-                "text"          =>  $say,
-                "username"      => $from,
-                "icon_emoji"    =>  $icon,
-                "attachments"  =>  array(array(
-                    "fallback" => 'Change summary',
-                    "color"    => '#333',
-                    "title"    => $INFO['id'],
-                    "title_link"=> $this->urlize(),
-                    "text"     => $summary,
-                    "author"   => $fullname
-                    ))
-            ));
-			
+		// encode data
+		$json = json_encode($data);
+
+		// init curl
+		$webhook = $this->getConf('webhook');
 		$ch = curl_init($webhook);
-			
-		$proxy = $conf['proxy'];
-		
-		// If DokuWiki is set up with proxy settings then use them in our cURL request
-		if (!empty($proxy)) {
-			// Configure the proxy address and port
+
+		// use proxy if defined
+		$proxy = $conf['proxy'];		
+		if (!empty($proxy['host'])) {
+
+			// configure proxy address and port
 			$proxyAddress = $proxy['host'] . ':' . $proxy['port'];
-			
+
 			curl_setopt($ch, CURLOPT_PROXY, $proxyAddress);
 			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			// TODO: This may be required internally but best to add a config flag/path to local certificate file
+			// TODO: may be required internally but best to add a config flag/path to local certificate file
 			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-			
-			// Only include a username and password if we actually have one
+		
+			// include username and password if defined
 			if (!empty($proxy['user']) && !empty($proxy['pass'])) {
 				$proxyAuth = $proxy['user'] . ':' . conf_decodeString($proxy['port']);
-				
 				curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyAuth);
 			}
-			
+
 		}
     
-        // You can get your webhook endpoint from your Slack settings
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        $result = curl_exec($ch);
+		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+		curl_setopt($ch, CURLOPT_POSTFIELDS, array('payload' => $json));
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		$result = curl_exec($ch);
 		
-		// This should ideally display or log only for Admin users/in debugging mode
+		// ideally display only for Admin users and/or in debugging mode
 		if ($result === false){
 			echo 'cURL error when posting Wiki save notification to Slack: ' . curl_error($ch);
 		}
 		
-        curl_close($ch);
-    }
+		curl_close($ch);
 
-    /* Make our URLs! */
-    private function urlize() {
+	}
 
-        global $INFO;
-        global $conf;
-        $page = $INFO['id'];
+	private function urlize($diffRev=null) {
 
-        switch($conf['userewrite']) {
-            case 0:
-                $url = DOKU_URL . "doku.php?id=" . $page;
-                break;
-            case 1:
-                if ($conf['useslash']) {
-                    $page = str_replace(":", "/", $page);
-                }
-                $url = DOKU_URL . $page;
-                break;
-            case 2:
-                if ($conf['useslash']) {
-                    $page = str_replace(":", "/", $page);
-                }
-                $url = DOKU_URL . "doku.php/" . $page;
-                break;
-        }
-        return $url;
-    }
+		global $conf;
+		global $INFO;
+
+		switch($conf['userewrite']) {
+			case 0:
+				if (!empty($diffRev)) {
+                        		$url = DOKU_URL . "doku.php?id={$INFO['id']}&rev={$diffRev}&do=diff";
+                		} else {
+                        		$url = DOKU_URL . "doku.php?id={$INFO['id']}";
+                		}
+				break;
+			case 1:
+				$id = $INFO['id'];
+				if ($conf['useslash']) {
+					$id = str_replace(":", "/", $id);
+				}
+				if (!empty($diffRev)) {
+                        		$url = DOKU_URL . "{$id}?rev={$diffRev}&do=diff";
+                		} else {
+                        		$url = DOKU_URL . $id;
+                		}
+				break;
+			case 2:
+				$id = $INFO['id'];
+				if ($conf['useslash']) {
+					$id = str_replace(":", "/", $id);
+				}
+				if (!empty($diffRev)) {
+                        		$url = DOKU_URL . "doku.php/{$id}?rev={$diffRev}&do=diff";
+                		} else {
+                        		$url = DOKU_URL . "doku.php/{$id}";
+                		}	
+				break;
+		}
+		return $url;
+	}
 }
 
 // vim:ts=4:sw=4:et:enc=utf-8:
