08e3086e8bb1728450b6fd3f98dc3cbad364350f
mallchin <mallchin@mac.com>
2016-06-09 09:38:01 +0100

    Update default.php

--- conf/default.php
+++ conf/default.php
@@ -1,11 +1,3 @@
 <?php
-/**
- * Default config options
- *
- * @author Jeremy Ebler <jebler@gmail.com>
- */
-
-  $conf['slackhq_icon']    = ":ghost:";
-  $conf['slackhq_channel']  = "<channel-name>";
-  $conf['slackhq_name']     = "DokuWiki";
-  $conf['slackhq_webhook']  = "https://hooks.slack.com/services/bla/bla/bla";
+    $conf['namespaces'] = "";
+    $conf['webhook']  = "https://hooks.slack.com/services/<token>";
