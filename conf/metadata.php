f35b90fca83106b76f6fe5dea606c682ec32a5fa
mallchin <mallchin@mac.com>
2016-06-09 09:38:29 +0100

    Update metadata.php

--- conf/metadata.php
+++ conf/metadata.php
@@ -1,12 +1,3 @@
 <?php
-/**
- * Metadata
- *
- * @author Brendon Rapp <brendon@jaguardesignstudio.com>
- */
-
-  $meta['slackhq_icon']      = array('string');
-  $meta['slackhq_channel']       = array('string');
-  $meta['slackhq_name']       = array('string');
-  $meta['slackhq_namespaces'] = array('string');
-  $meta['slackhq_webhook']       = array('string');
+  $meta['namespaces'] = array('string');
+  $meta['webhook'] = array('string');
