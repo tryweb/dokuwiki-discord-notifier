a21a61338f33af3b4b9d7475ebff3ee2ca95d171
zteeed <zteeed@minet.net>
2019-08-17 23:52:36 +0000

    Add example to send an embed message using discord webhook

---
+++ examples/send_embeds.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * https://birdie0.github.io/discord-webhooks-guide/structure/embed/color.html
+ */
+
+$conf = array();
+include "../conf/default.php";
+
+function send_embeds($webhook)
+{
+    $title = "This is a title.";
+    $color = hexdec("00cccc");
+    $url = "https://github.com/zteeed/dokuwiki-discord-notifier";
+    $description = "*Hi!* **Wow!** I can __use__ hyperlinks and star this project [here]($url).";
+    $footer = array("text" => "Dokuwiki discordnotifier v1.0.0");
+
+    // format POST data
+    $payload = array("embeds" =>
+        array(
+            array("title" => $title, "color" => $color, "description" => $description, "footer" => $footer)
+        ),
+    );
+    $json = json_encode($payload);
+    print_r($json);
+    // init curl
+    $ch = curl_init($webhook);
+    // submit payload
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_exec($ch);
+    // close curl
+    Curl_close($ch);
+}
+
+$webhook = $conf['webhook'];
+send_embeds($webhook);
