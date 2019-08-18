b5d35c700fcab6bbab1265ce8ccc66474856b05e
zteeed <zteeed@minet.net>
2019-08-18 00:44:33 +0000

    Add french support for settings

---
+++ lang/fr/settings.php
@@ -0,0 +1,7 @@
+<?php
+$lang['namespaces'] = "Espaces de noms (séparés par des virgules)";
+$lang['notify_create'] = "Notifier lorsque des pages sont créées";
+$lang['notify_edit'] = "Notifier quand les pages sont éditées";
+$lang['notify_delete'] = "Notifier lorsque des pages sont supprimées";
+$lang['show_summary'] = "Afficher le résumé s'il en existe un";
+$lang['webhook'] = "URL du Webhook Discord";
