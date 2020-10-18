0f37f48b6cab9e0b7d1ac8cd4805321a535c8c4f
Rainbow Spike <3947636+Rainbow-Spike@users.noreply.github.com>
2020-10-18 21:08:36 +1000

    New subversion number
    I think the new plugin structure is worthy of getting a higher subversion number

--- helper.php
+++ helper.php
@@ -112,7 +112,7 @@ class helper_plugin_discordnotifier extends DokuWiki_Plugin {
             if ( $summary ) $description .= "\n" . $lang['summary'] . ": " . $summary;
         }
         
-        $footer = array ( "text" => "Dokuwiki DiscordNotifier v1.0.3" );
+        $footer = array ( "text" => "Dokuwiki DiscordNotifier v1.1.0" );
         $payload = array ( "embeds" =>
             array (
                 ["title" => $title, "color" => $embed_color, "description" => $description, "footer" => $footer]
