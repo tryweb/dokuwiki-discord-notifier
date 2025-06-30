52f3afddeb8b74546bd8aaec14aec7741623a41a
Jonathan Tsai <tryweb@ichiayi.com>
2025-06-30 16:40:59 +0800

    This change ensures proper Traditional Chinese (zh-TW) localization for the DokuWiki Discord Notifier plugin.

---
+++ lang/zh-tw/lang.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl-2.0.html)
+ * 
+ * @author Jonathan Tsai <tryweb@ichiayi.com>
+ */
+
+$lang['t_created']   = '已建立';
+$lang['t_updated']   = '已更新';
+$lang['t_minor_upd'] = '小修改';
+$lang['t_removed']   = '已移除';
+
+$lang['e_created']   = '建立了頁面';
+$lang['e_updated']   = '更新了頁面';
+$lang['e_minor_upd'] = '對頁面做了小修改';
+$lang['e_removed']   = '移除了頁面';
+
+$lang['compare']     = '比較差異';
---
+++ lang/zh-tw/settings.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl-2.0.html)
+ * 
+ * @author Jonathan Tsai <tryweb@ichiayi.com>
+ */
+
+$lang['namespaces'] ="命名空間（以半形逗號分隔）";
+$lang['notify_create'] ="建立頁面時發出通知";
+$lang['notify_edit'] ="編輯頁面時發出通知";
+$lang['notify_edit_minor'] ="即使頁面有較小的更改也要發出通知";
+$lang['notify_delete'] ="刪除頁面時發出通知";
+$lang['notify_show_summary'] ="編輯頁面時顯示摘要";
+$lang['notify_show_name'] = "在通知中顯示的名稱";
+$lang['webhook'] ="Discord 頻道的 Webhook 網址";
